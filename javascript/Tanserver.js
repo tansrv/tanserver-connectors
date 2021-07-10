1949d39cf12316af06beec13f443e64dadf5f104
DaniAffCH <danieleaffinita2000@gmail.com>
2021-07-10 12:37:14 +0200

    removed class, completed but not tested yet

--- javascript/Tanserver.js
+++ javascript/Tanserver.js
@@ -1,28 +1,82 @@
-class Tanserver
-{
-    #host;
-    #port;
-    #worker;
 
-    /**
-     * @param {String}  host Hostname of tanserver
-     * @param {Integer} port Port of tanserver 
-     */
-    constructor(host, port)
-    {
-        this.#host = host;
-        this.#port = port;
-    }
+//use encapsulation to prevent confilct with user code
+
+/**
+ * @param {String}  host Hostname of tanserver
+ * @param {Integer} port Port of tanserver 
+ */
+function Tanserver(host, port){
+    let worker;
+    let _this = this;
 
     /**
      * @param  {Function} func Function that must run in background thread
      * @return {Worker}        Return a Worker instance executing func
      */
-    #createWorker(func)
-    {
+    let createWorker = (func) => {
         var blob = new Blob(['self.onmessage = ', func.toString()], { type: 'text/javascript' });
         var url = URL.createObjectURL(blob);
         return new Worker(url);
+    }
+
+    /**
+     * @param  {String} userApi    API name
+     * @param  {String} jsonString Parameter of API as json 
+     * @return {String} return header {"user_api":"...","json_length":"..."}\r\n
+     */
+    let makeHeader = (userApi, jsonString) => {
+        return `{"user_api":"${userApi}","json_length":"${jsonString}"}\r\n`;
+    }
+
+    /**
+     * @param  {String} userApi    API name
+     * @param  {String} jsonString Parameter of API as json 
+     * @return {String} return packet using custom protocol
+     */    
+    let makePacket = (userApi, jsonString) => {
+        return makeHeader(userApi, jsonString.length) + jsonString;
+    }
+
+    let WorkerActions = (e) => {
+        if(e.data.action == "run")
+        {
+            //self.postMessage("success") if json is obtained successfully
+            //self.postMessage("failure") otherwise
+            const ws = undefined;
+
+            try 
+            {
+                ws = new WebSocket(`wss://${e.data.host}:${e.data.port}`);
+            }
+            catch(err) 
+            {
+                console.log(err);
+                self.postMessage("failure");
+                return;
+            }
+            
+            ws.addEventListener("open", function(e){
+                try 
+                {
+                    ws.send(makePacket(userApi, jsonString));
+                }
+                catch(err)
+                {
+                    ws.close("Unable to send packet");
+                    console.log(err);
+                    self.postMessage("failure");
+                    return;
+                }
+            });
+
+            ws.addEventListener('message', function (e) {
+                ws.close(1000);
+                console.log('Message from tanserver: ', e.data);
+                self.postMessage("success");
+                return;
+            });
+            
+        }
     }
 
     /**
@@ -31,35 +85,14 @@ class Tanserver
      * @param {Function} successCallback (Optional) Function executed if json is obtained successfully 
      * @param {Function} failureCallback (Optional) Function executed if getJSON fails
      */
-    getJSON(userApi, jsonString, successCallback = undefined, failureCallback = undefined)
-    {    
-        if(typeof(this.#worker) == "undefined") 
+     _this.getJSON = (userApi, jsonString, successCallback = undefined, failureCallback = undefined) =>{
+        if(typeof(worker) == "undefined") 1000
         {
-            this.#worker = this.#createWorker(
-                function (e){
-                    if(e.data.action == "run")
-                    {
-                        //self.postMessage("success") if json is obtained successfully
-                        //self.postMessage("failure") otherwise
-                        
-                        //TODO: throw connection error and return failure
-                        const ws = new WebSocket(`wss://${e.data.host}:${e.data.port}`);
-                        
-                        ws.addEventListener("open", function(e){
-                            ws.send(this.makePacket(userApi, jsonString));
-                        });
-
-                        ws.addEventListener('message', function (e) {
-                            console.log('Message from tanserver: ', e.data);
-                        });
-                        
-                    }
-                }
-            );
+            worker = createWorker(WorkerActions);
         }
 
         // handle answer from worker
-        this.#worker.onmessage = function (e){
+        worker.onmessage = function (e){
             if(e.data == "success")
             {
                 if(successCallback != undefined)
@@ -76,20 +109,8 @@ class Tanserver
         }
 
         // send a message to worker
-        this.#worker.postMessage({'action':'run',
-                                  'host':this.#host,
-                                  'port':this.#port});
-
+        worker.postMessage({'action':'run',
+                            'host':host,
+                            'port':port});
     }
-
-    /**
-     * @param  {String} userApi    API name
-     * @param  {String} jsonString Parameter of API as json 
-     * @return {String} return packet using custom protocol
-     */    
-    makePacket(userApi, jsonString){
-        //TODO
-        return "hello world";
-    }
-
 }
